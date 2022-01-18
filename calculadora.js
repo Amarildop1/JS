196f28e02d506a821b84081bdb121bc9d5f8a665
Amarildo <amarildop1dev@gmail.com>
2022-01-18 15:03:42 -0300

    Calculadora básica

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+let dividir = require("./dividir.js");
+let multiplicar = require("./multiplicar.js");
+let somar = require("./somar.js");
+let subtrair = require("./subtrair.js");
+
+
+console.log(`Divisão: ${dividir(10, 2)}`);
+console.log(`Multiplicação: ${multiplicar(10, 2)}`);
+console.log(`Somar: ${somar(10, 2)}`);
+console.log(`Subtrair: ${subtrair(10, 2)}`);
