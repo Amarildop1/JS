dcf3355e7621a92e4645c46d6e06a469df1da896
Amarildo <amarildop1dev@gmail.com>
2022-02-11 11:00:50 -0300

    Métodos get, post, put, delete | Com NodeJS, Express, JavaScript e Typescript

--- RESTcomNodeJS/src/index.ts
+++ RESTcomNodeJS/src/index.ts
@@ -2,16 +2,20 @@ import express, { Request, Response, NextFunction } from "express";
 import usersRoute from "./routes/users.route";
 
 const app = express();
-
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+/* app.get('/status', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send("<h1> Hello World! </h1> <h3> com NodeJS, JavaScript, TypeScript e Express </h3>");
-});
+}); */
 
 
+//Configurações da Aplicação
+app.use(express.json());
+
+
+//Configurações de Rotas
 app.use(usersRoute);
 
 
-
+//Inicialização do servidor
 app.listen(3000, () => {
     console.log("Servidor rodando em http://localhost:3000");
 });
--- RESTcomNodeJS/src/routes/users.route.ts
+++ RESTcomNodeJS/src/routes/users.route.ts
@@ -2,15 +2,30 @@ import { Router, Request, Response, NextFunction } from "express";
 
 const usersRoute = Router();
 
-usersRoute.get("/", (req: Request, res: Response, next: NextFunction) => {
-    const users = [ { name: "John" } ];
-    res.status(200).send(users);
+usersRoute.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send("<h1> Hello World! </h1> <h3> com NodeJS, JavaScript, TypeScript e Express </h3>");
+});
+
+usersRoute.get("/users", (req: Request, res: Response, next: NextFunction) => {
+    const users = [
+        { userName: "John", idade: 100 },
+        { userName: "Maria", idade: 98 }
+    ];
+    res.status(200).send( users );
 });
 
 usersRoute.get("/users/:id", (req: Request, res: Response, next: NextFunction) => {
     const idUser = req.params.id;
     res.status(200).send( { idUser } );
 });
+
+
+usersRoute.post("/users", (req: Request, res: Response, next: NextFunction) => {
+    const novoUser = req.body;
+    res.status(201).send( novoUser );
+});
+
+
 
 
 export default usersRoute;
